0974ebc2383240d9ae817857ccaf2819f2a0a86e
Jason <turquoiseluv@naver.com>
2020-01-20 00:22:02 +0900

    creating transparent loading screen (not optimized)

--- app/src/main/java/com/example/winners_app/RegisterActivity.java
+++ app/src/main/java/com/example/winners_app/RegisterActivity.java
@@ -2,15 +2,25 @@ package com.example.winners_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Button back;
     Button reg;
+    Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +40,50 @@ public class RegisterActivity extends AppCompatActivity {
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 로딩 팝업창
-                Intent intent = new Intent();
-                intent.setClass(RegisterActivity.this, LoadingPopup.class);
-                startActivity(intent);
+                Dialog dialog = new Dialog(RegisterActivity.this);
+                LinearLayout contentView = (LinearLayout) (RegisterActivity.this).getLayoutInflater().inflate(R.layout.activity_loading_popup, null);
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(contentView);
 
+                ImageView image = (ImageView) contentView.findViewById(R.id.imageView);
+                final AnimationDrawable animation = (AnimationDrawable) image.getDrawable();
+                dialog.setCancelable(false);
+
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        animation.start();
+                    }
+                });
+                dialog.show();
+
+                // DB 연동 시간 설정 (일단 5초)
+                thread = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            synchronized (this) {
+                                // Wait given period of time or exit on touch
+                                wait(5000);
+                            }
+                        } catch (InterruptedException ex) {
+                        }
+                        // 끝나면,
+                        finish();
+                        // 로그인 화면으로 돌아가기
+                        Intent intent = new Intent();
+                        intent.setClass(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                };
+                thread.start();
+
+                //
                 // DB 연동...
-
-                finish();
+                //
             }
         });
     }
 }
+
